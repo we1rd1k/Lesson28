3a54b81615c29301f8b09825b9f3d4ebe0cf6208
deNNi <dns.nikonorov@gmail.com>
2021-03-04 12:03:06 +0400

    Add logger slf4j

--- src/main/java/innopolis/ui/google/GoogleMainPage.java
+++ src/main/java/innopolis/ui/google/GoogleMainPage.java
@@ -6,30 +6,37 @@ import innopolis.ui.Props;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class GoogleMainPage extends MainPage {
 
     private static Props props = ConfigFactory.create(Props.class);
+    private static Logger LOG = LoggerFactory.getLogger(GoogleMainPage.class);
 
     @Override
     public void openPage() {
+        LOG.info("Открываем главную страницу Google");
         open(props.googleUrl());
     }
 
     @Override
     public void fillInSearchField(String request) {
+        LOG.info("Вводим в строку поиска {}", request);
         $(By.name("q")).setValue(request);
     }
 
     @Override
     public void searchButtonClick() {
+        LOG.info("Кликаем кнопку поиска");
         $(By.name("btnK")).click();
     }
 
     @Override
     public void checkPageTitle(String title) {
+        LOG.info("Проверяем, что в заголовке присутствует {}", title);
         super.checkPageTitle(title);
     }
 }
--- src/main/java/innopolis/ui/yandex/YandexMainPage.java
+++ src/main/java/innopolis/ui/yandex/YandexMainPage.java
@@ -3,9 +3,12 @@ package innopolis.ui.yandex;
 import com.codeborne.selenide.Condition;
 import innopolis.ui.MainPage;
 import innopolis.ui.Props;
+import innopolis.ui.google.GoogleMainPage;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$x;
@@ -13,24 +16,29 @@ import static com.codeborne.selenide.Selenide.$x;
 public class YandexMainPage extends MainPage {
 
     private static Props props = ConfigFactory.create(Props.class);
+    private static Logger LOG = LoggerFactory.getLogger(YandexMainPage.class);
 
     @Override
     public void openPage() {
+        LOG.info("Открываем главную страницу Yandex");
         open(props.yandexUrl());
     }
 
     @Override
     public void fillInSearchField(String request) {
+        LOG.info("Вводим в строку поиска {}", request);
         $(By.name("text")).setValue(request);
     }
 
     @Override
     public void searchButtonClick() {
+        LOG.info("Кликаем кнопку поиска");
         $x("//button[@type='submit']").click();
     }
 
     @Override
     public void checkPageTitle(String title) {
+        LOG.info("Проверяем, что в заголовке присутствует {}", title);
         super.checkPageTitle(title);
     }
 }
